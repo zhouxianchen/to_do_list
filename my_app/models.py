79ba0554dfa2e6b86962e5863456534e051fca0f
zhouxianchen <zhouxianchen13@163.com>
2020-03-11 19:06:21 +0800

    基本模型有了

--- my_app/models.py
+++ my_app/models.py
@@ -7,3 +7,7 @@ class One_task(models.Model):
     sub_task = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
     jindu = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.task+":"+self.sub_task)
+
--- my_app/views.py
+++ my_app/views.py
@@ -1,34 +1,49 @@
 from django.shortcuts import render,redirect
-
+from .models import One_task
 # Create your views here.
-my_database = [
-{'添加大类': "管理工作"}
-]
+
+
 
 def home(request):
     global my_database
     if request.method == "POST":
-
         if request.POST['添加计划'] == '':
-            return render(request, "home.html", {'警告':'请输入内容'})
+            content = {"清单": One_task.objects.all()}
+            return render(request, "home.html", {'警告':'请输入内容'}, content)
         else:
-            my_database.append({'添加大类': request.POST["添加计划"]})
-            content = {"清单": my_database}
+            a_row = One_task(big_subject=request.POST['添加计划'], task="学法规 ", sub_task="实施考核", time="2019", jindu="30%")
+            a_row.save()
+            content = {"清单": One_task.objects.all()}
             return render(request, "home.html", content)
     elif request.method == "GET":
-        content = {"清单": my_database}
+        content = {"清单": One_task.objects.all()}
         return render(request, "home.html", content)
 
 
 
-def edit(request):
+def edit(request, plan_id):
     return render(request, "edit.html")
 
 
 def about(request):
-    return render(request, "about.html")
+    if request.method == "POST":
+        if request.POST['添加计划'] == '':
+            content = {"清单": One_task.objects.all()}
+            return render(request, "about.html", {'警告': '请输入内容'}, content)
+        else:
+            a_row = One_task(big_subject=request.POST['添加计划'], task="学法规 ", sub_task="实施考核", time="2019", jindu="30%")
+            a_row.save()
+            content = {"清单": One_task.objects.all()}
+            return render(request, "about.html", content)
 
 
-def delete(request, forloop_counter):
-    my_database.pop(int(forloop_counter)-1)
-    return redirect("my_app:主页")
+
+    if request.method == "GET":
+        content = {"清单": One_task.objects.all()}
+        return render(request, "about.html", content)
+
+
+
+def delete(request, plan_id):
+    One_task.objects.get(id=plan_id).delete()
+    return redirect("my_app:关于")
