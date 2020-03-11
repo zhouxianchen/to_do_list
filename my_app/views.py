ce528cc6e9affd6ec23c2057f03be1723feb9db3
zhouxianchen <zhouxianchen13@163.com>
2020-03-11 19:53:01 +0800

    改回来再看看

--- my_app/views.py
+++ my_app/views.py
@@ -24,18 +24,21 @@ def home(request):
 def edit(request):
     return render(request, "edit.html")
 
+
 def about(request):
 <<<<<<< HEAD
     if request.method == "POST":
-        # request.POST['big_subject']
-        if request.POST['添加大类'] == "":
+        if request.POST['添加计划'] == '':
             content = {"清单": One_task.objects.all()}
             return render(request, "about.html", {'警告': '请输入内容'}, content)
         else:
-            a_row = One_task(big_subject=request.POST['big_subject'], task="学法规 ", sub_task="实施考核", time="2019", jindu="30%")
+            a_row = One_task(big_subject=request.POST['添加计划'], task="学法规 ", sub_task="实施考核", time="2019", jindu="30%")
             a_row.save()
             content = {"清单": One_task.objects.all()}
             return render(request, "about.html", content)
+
+
+
     if request.method == "GET":
         content = {"清单": One_task.objects.all()}
         return render(request, "about.html", content)
@@ -43,25 +46,6 @@ def about(request):
 =======
     return render(request, "about.html")
 >>>>>>> parent of 79ba055... 基本模型有了
-
-# def about(request):
-#     if request.method == "POST":
-#         big_subject1 = request.POST['big_subject']
-#         task1 = request.POST['task']
-#         sub_task1 = request.POST['sub_task']
-#         time1 = request.POST['time']
-#         jindu1 = request.POST['jindu']
-#         if big_subject1 or task1 or sub_task1 or time1 or jindu1 == "":
-#             content = {"清单": One_task.objects.all()}
-#             return render(request, "about.html", {'警告': '请输入内容!'}, content)
-#         else:
-#             a_row = One_task(big_subject=big_subject1, task=task1, sub_task=sub_task1, time=time1, jindu=jindu1)
-#             a_row.save()
-#             content = {"清单": One_task.objects.all()}
-#             return render(request, "about.html", content)
-#     if request.method == "GET":
-#         content = {"清单": One_task.objects.all()}
-#         return render(request, "about.html", content)
 
 
 def delete(request, forloop_counter):
