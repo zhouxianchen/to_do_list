22979c45cdced9891c9af2115993ea85447c0921
zhouxianchen <zhouxianchen13@163.com>
2020-03-12 21:06:52 +0800

    尝试修改了这真个模型

--- my_app/models.py
+++ my_app/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 
 # Create your models here.
-class One_task(models.Model):
-    BIG_CHOICES = (
-        ('FR', 'Freshman'),
-        ('SO', 'Sophomore'),
-        ('JR', 'Junior'),
-        ('SR', 'Senior'),
-        ('GR', 'Graduate'),
-    )
-    big_subject = models.CharField(max_length=50, choices=BIG_CHOICES)
-    task = models.CharField(max_length=50)
-    sub_task = models.CharField(max_length=50)
-    time = models.CharField(max_length=50)
-    jindu = models.CharField(max_length=50)
+# class One_task(models.Model):
+#     BIG_CHOICES = (
+#         ('FR', 'Freshman'),
+#         ('SO', 'Sophomore'),
+#         ('JR', 'Junior'),
+#         ('SR', 'Senior'),
+#         ('GR', 'Graduate'),
+#     )
+#     big_subject = models.CharField(max_length=50, choices=BIG_CHOICES)
+#     task = models.CharField(max_length=50)
+#     sub_task = models.CharField(max_length=50)
+#     time = models.CharField(max_length=50)
+#     jindu = models.CharField(max_length=50)
+
+
+class Big_subject(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    big_subject = models.ForeignKey(to='Big_subject', on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    task = models.ForeignKey(to='Task', on_delete=models.CASCADE)
+    progress = models.CharField(max_length=50)
+    start_time = models.DateField(max_length=50, blank=True)
+    end_time = models.DateField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
--- my_app/views.py
+++ my_app/views.py
@@ -1,60 +1,64 @@
 from django.shortcuts import render,redirect
-from .models import One_task
+from .models import Big_subject, Task, Activity
 # Create your views here.
 
 # Create your views here.
 
 
 def home(request):
-    if request.method == "POST":
-        if request.POST['添加计划'] == '':
-            return render(request, "home.html", {'警告':'请输入内容'})
-        else:
-            content = {"清单": One_task.objects.all()}
-            return render(request, "home.html", content)
-    elif request.method == "GET":
-        content = {"清单": One_task.objects.all()}
+    # if request.method == "POST":
+    #     if request.POST['添加计划'] == '':
+    #         return render(request, "home.html", {'警告':'请输入内容'})
+    #     else:
+    #         content = {"清单": Activity.objects.all()}
+    #         return render(request, "home.html", content)
+    if request.method == "GET":
+        content = {"清单": Activity.objects.all()}
         return render(request, "home.html", content)
 
 
 
 def edit(request, plan_id):
     if request.method == "POST":
-        cur =One_task.objects.get(id=plan_id)
-        cur.big_subject =request.POST['big_subject']
+        cur = Activity.objects.get(id=plan_id)
+        cur.task.big_subject = request.POST['big_subject']
         cur.task = request.POST['task']
-        cur.sub_task =request.POST['sub_task']
-        cur.time = request.POST['time']
-        cur.jindu = request.POST['jindu']
+        cur.name =request.POST['activity']
+        cur.start_time = request.POST['start_time']
+        cur.end_time = request.POST['end_time']
+        cur.progress = request.POST['progress']
         cur.save()
-        content = {"清单": One_task.objects.all()}
+        content = {"清单": Activity.objects.all()}
         return redirect("my_app:关于")
     elif request.method =="GET":
-        current_task = {"current": One_task.objects.get(id=plan_id)}
+        current_task = {"current": Activity.objects.get(id=plan_id)}
         return render(request, "edit.html", current_task)
 
 
 def about(request):
 
     if request.method == "POST":
-        big_subject = request.POST['big_subject']
-        task = request.POST['task']
-        sub_task = request.POST['sub_task']
-        time = request.POST['time']
-        jindu = request.POST['jindu']
+        new_big_subject_id = request.POST['big_subject']
+        new_task_id = request.POST['task']
+        activity = request.POST['activity']
+        start_time = request.POST['start_time']
+        end_time = request.POST['end_time']
+        progress = request.POST['progress']
 
-        a_row = One_task(big_subject=big_subject, task=task, sub_task=sub_task, time=time, jindu=jindu)
-        a_row.save()
-        content = {"清单": One_task.objects.all(),'select_form': One_task.BIG_CHOICES}
+        task_row = Task(name=new_task_id, big_subject_id=new_big_subject_id)
+        task_row.save()
+        act_row = Activity(name=activity, task_id=new_task_id, start_time=start_time, end_time=end_time, progress=progress)
+        act_row.save()
+        content = {"清单": Activity.objects.all()}
         return render(request, "about.html", content)
 
     if request.method == "GET":
-        content = {"清单": One_task.objects.all(), 'select_form': One_task.BIG_CHOICES}
+        content = {"清单": Activity.objects.all()}
         return render(request, "about.html", content)
 
     return render(request, "about.html")
 
 
-def delete(request, plan_id):
-    One_task.objects.get(id=plan_id).delete()
+def delete(request, activity_id):
+    Activity.objects.get(id=activity_id).delete()
     return redirect("my_app:关于")
