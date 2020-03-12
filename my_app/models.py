421570db4d415329f2e12b4f9496ecb7007091a7
zhouxianchen <zhouxianchen13@163.com>
2020-03-12 11:05:45 +0800

    基本成型的模型

--- my_app/models.py
+++ my_app/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class One_task(models.Model):
-    big_subject = models.CharField(max_length=50)
+    BIG_CHOICES = (
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+    )
+    big_subject = models.CharField(max_length=50, choices=BIG_CHOICES)
     task = models.CharField(max_length=50)
     sub_task = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
--- my_app/views.py
+++ my_app/views.py
@@ -1,68 +1,60 @@
 from django.shortcuts import render,redirect
-<<<<<<< HEAD
 from .models import One_task
 # Create your views here.
-=======
 
 # Create your views here.
-my_database = [
-{'添加大类': "管理工作"}
-]
->>>>>>> parent of 79ba055... 基本模型有了
+
 
 def home(request):
     if request.method == "POST":
-
         if request.POST['添加计划'] == '':
             return render(request, "home.html", {'警告':'请输入内容'})
         else:
-<<<<<<< HEAD
-=======
-            my_database.append({'添加大类': request.POST["添加计划"]})
->>>>>>> parent of 79ba055... 基本模型有了
-            content = {"清单": my_database}
+            content = {"清单": One_task.objects.all()}
             return render(request, "home.html", content)
     elif request.method == "GET":
-        content = {"清单": my_database}
+        content = {"清单": One_task.objects.all()}
         return render(request, "home.html", content)
 
 
 
-def edit(request):
-    return render(request, "edit.html")
+def edit(request, plan_id):
+    if request.method == "POST":
+        cur =One_task.objects.get(id=plan_id)
+        cur.big_subject =request.POST['big_subject']
+        cur.task = request.POST['task']
+        cur.sub_task =request.POST['sub_task']
+        cur.time = request.POST['time']
+        cur.jindu = request.POST['jindu']
+        cur.save()
+        content = {"清单": One_task.objects.all()}
+        return redirect("my_app:关于")
+    elif request.method =="GET":
+        current_task = {"current": One_task.objects.get(id=plan_id)}
+        return render(request, "edit.html", current_task)
 
 
 def about(request):
-<<<<<<< HEAD
 
     if request.method == "POST":
-        if request.POST['添加计划'] == '':
-            content = {"清单": One_task.objects.all()}
-            return render(request, "about.html", {'警告': '请输入内容'}, content)
-        else:
-            a_row = One_task(big_subject=request.POST['添加计划'], task="学法规 ", sub_task="实施考核", time="2019", jindu="30%")
-            a_row.save()
-            content = {"清单": One_task.objects.all()}
-            return render(request, "about.html", content)
+        big_subject = request.POST['big_subject']
+        task = request.POST['task']
+        sub_task = request.POST['sub_task']
+        time = request.POST['time']
+        jindu = request.POST['jindu']
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> parent of 6408944... 添加输入功能（有bug）
-    if request.method == "GET":
-        content = {"清单": One_task.objects.all()}
+        a_row = One_task(big_subject=big_subject, task=task, sub_task=sub_task, time=time, jindu=jindu)
+        a_row.save()
+        content = {"清单": One_task.objects.all(),'select_form': One_task.BIG_CHOICES}
         return render(request, "about.html", content)
 
+    if request.method == "GET":
+        content = {"清单": One_task.objects.all(), 'select_form': One_task.BIG_CHOICES}
+        return render(request, "about.html", content)
 
-<<<<<<< HEAD
-=======
     return render(request, "about.html")
 
-=======
->>>>>>> parent of 6408944... 添加输入功能（有bug）
 
->>>>>>> parent of 79ba055... 基本模型有了
-def delete(request, forloop_counter):
-    my_database.pop(int(forloop_counter)-1)
-    return redirect("my_app:主页")
+def delete(request, plan_id):
+    One_task.objects.get(id=plan_id).delete()
+    return redirect("my_app:关于")
